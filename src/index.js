8cf56504525125be4e75574585615eba0676b137
saryCast <cast.sara@gmail.com>
2024-12-05 16:36:29 -0300

    hora actual en react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+/* const element = document.createElement("h1"); //creamos elemento
+element.innerText="JovenesProgramadores"; //el texto que vamos a colocar
+const container = document.getElementById('root'); //creamos variable que obtiene la ID
+container.appendChild(element); //que queremos mostrar
+*/
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*
+const name = 'sara'
+const edad= 21*2;
+const element = <h1>{name} de {edad} años está aprendiendo REACT en JovenesProgramadores</h1>;
+const container = document.getElementById("root");
+ReactDom.render(element, container);
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const style = {
+  color: "red"
+}
+
+function tictac(){
+  const element=(
+    <div>
+      <h1 style={style}>Reloj en React!</h1>
+      <h2>Son las {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  const container = document.getElementById("root");
+  ReactDom.render(element, container);
+}
+setInterval(tictac,1000);
